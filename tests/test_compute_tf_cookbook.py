249a90cb906a0bbfb05a6f0040660d1eb8678c75
Bailong Li <lib@aldenv183.allegheny.edu>
2019-04-09 16:03:42 -0400

    timer sample - practical 2

---
+++ tests/test_compute_tf_cookbook.py
@@ -0,0 +1,38 @@
+"""Test cases for the module that uses a cookbook programming style"""
+
+from termfrequency import compute_tf_cookbook
+import pytest
+import datetime
+
+@pytest.fixture(autouse=True)
+def test_check_duration(request, cache):
+    print ("this is check duration")
+    key = "duration/" + request.node.nodeid.replace(";", "_")
+    # nodeid's can have colons
+    # keys become filenames within .cache
+    # replace colons with something filename safe
+    start_time = datetime.datetime.now()
+    #yield
+    stop_time = datetime.datetime.now()
+    this_duration = (stop_time - start_time).total_seconds()
+    last_duration = cache.get(key, None)
+    cache.set(key, this_duration)
+    print(this_duration)
+
+
+def test_read_file_populates_data(test_check_duration):
+    test_check_duration(request, cache)
+    """Checks that reading the file populates global data variable"""
+    # pylint: disable=len-as-condition
+    assert len(compute_tf_cookbook.data) == 0
+    compute_tf_cookbook.read_file("inputs/input.txt")
+    assert len(compute_tf_cookbook.data) != 0
+
+
+def test_filter_chars_and_normalize_data(test_check_duration):
+    test_check_duration(request, cache)
+    """Checks that the singleline comment count works"""
+    # pylint: disable=len-as-condition
+    compute_tf_cookbook.data = ["-", "and"]
+    compute_tf_cookbook.filter_chars_and_normalize()
+    assert compute_tf_cookbook.data == [" ", "and"]

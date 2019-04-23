509a7cdbe3da379787c35e80da0c0ab734b7005c
Dillon <31478969+thomad74@users.noreply.github.com>
2019-04-23 15:15:24 -0400

    Update pytest_speedy.py

--- pytest_speedy.py
+++ pytest_speedy.py
@@ -18,7 +18,7 @@ def pytest_report_header():
     return msg
 
    
- def pytest_collect_file(path, parent):
-    """ Hook to save duration of tests in a txt file """
-  if path.ext == '.py':
-    return PyCollector(path=path, parent=parent)
+#  def pytest_collect_file(path, parent):
+#     """ Hook to save duration of tests in a txt file """
+#   if path.ext == '.py':
+#     return PyCollector(path=path, parent=parent)

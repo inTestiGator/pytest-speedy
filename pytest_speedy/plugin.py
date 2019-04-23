26d2334f1f9878b1ec5aeacd182e07b88ed04824
Matthew Marconi <marconim@aldenv185.allegheny.edu>
2019-04-23 16:16:43 -0400

    collection modifying items

--- pytest_speedy/plugin.py
+++ pytest_speedy/plugin.py
@@ -1,3 +1,11 @@
 import operator
 
 
+def pytest_collection_modifyitems(session, config, items):
+   """ called after collection has been performed, may filter or re-order
+   the items in-place.
+
+   :param _pytest.main.Session session: the pytest session object
+   :param _pytest.config.Config config: pytest config object
+   :param List[_pytest.nodes.Item] items: list of item objects
+   """

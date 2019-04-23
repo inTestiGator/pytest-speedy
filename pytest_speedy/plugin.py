70e5e74ee8b4cbc6e3f06a9372c11a747756a0ad
Matthew Marconi <marconim@aldenv185.allegheny.edu>
2019-04-23 16:17:41 -0400

    delete collection hook

--- pytest_speedy/plugin.py
+++ pytest_speedy/plugin.py
@@ -1,15 +1,6 @@
 import operator
 
 
-def pytest_collection_modifyitems(session, config, items):
-   """ called after collection has been performed, may filter or re-order
-   the items in-place.
-
-   :param _pytest.main.Session session: the pytest session object
-   :param _pytest.config.Config config: pytest config object
-   :param List[_pytest.nodes.Item] items: list of item objects
-   """
-
 Dict = {"test1": 3, "test2": 2, "test3": 1}
 print(Dict)
 

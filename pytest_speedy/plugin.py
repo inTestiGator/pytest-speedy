60cb0b1c5d6fb5c8dd214a97ff441be2cd455198
Matthew Marconi <marconim@aldenv185.allegheny.edu>
2019-04-23 16:18:15 -0400

    template for adding to a dictionary

--- pytest_speedy/plugin.py
+++ pytest_speedy/plugin.py
@@ -4,3 +4,16 @@ import operator
 Dict = {"test1": 3, "test2": 2, "test3": 1}
 print(Dict)
 
+def frequencies(word_list):
+    """
+    Takes a list of words and returns a dictionary associating
+    words with frequencies of occurrence
+    """
+    word_freqs = {}
+    for i in word_list:
+        if i in word_freqs:
+            word_freqs[i] += 1
+        else:
+            word_freqs[i] = 1
+    return word_freqs
+

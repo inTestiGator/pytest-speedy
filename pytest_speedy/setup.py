208e1d55cae572cc716ecd850d73ef1a8d7352dc
Matthew Marconi <marconim@aldenv138.allegheny.edu>
2019-04-30 14:25:20 -0400

    initial template for setup.py file

--- pytest_speedy/setup.py
+++ pytest_speedy/setup.py
@@ -1,3 +1,16 @@
 """Setup for pytest-speedy plugin"""
 
 from setuptools import setup
+
+setup(
+    name="pytest-speedy",
+    version="0.1.0",
+    description="A pytest plugin that reorders test cases from fastest to slowest.",
+    url="url here",
+    author="Pytest-Speedy Team",
+    author_email="email here",
+    license="Pytest-Speedy Copyright ",
+    py_modules=["pytest_speedy"],
+    install_requires=["pytest"],
+    entry_points={"pytest11": ["speedy = pytest_speedy"]},
+)

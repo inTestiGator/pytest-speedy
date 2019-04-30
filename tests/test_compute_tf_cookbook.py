52d65f44b3bbdeb9f86b4d4fb257e8cb9f468893
Dillon Thoma <thomad@aldenv139.allegheny.edu>
2019-04-30 16:02:40 -0400

    Removing unused imports

--- tests/test_compute_tf_cookbook.py
+++ tests/test_compute_tf_cookbook.py
@@ -1,10 +1,10 @@
 """Test cases for the module that uses a cookbook programming style"""
 
-import datetime
-import pytest
+# import datetime
+# import pytest
 from termfrequency import compute_tf_cookbook
 
-# use "pipenv run pytest -s" to run this fixture
+# # use "pipenv run pytest -s" to run this fixture
 # @pytest.fixture(autouse=True)
 # def test_check_duration(request, cache):
 #     """ Docstring """
@@ -37,3 +37,22 @@ def test_filter_chars_and_normalize_data():
     compute_tf_cookbook.data = ["-", "and"]
     compute_tf_cookbook.filter_chars_and_normalize()
     assert compute_tf_cookbook.data == [" ", "and"]
+
+
+def test_scan():
+    """Checks that the scan function works properly"""
+    # pylint: disable=len-as-condition
+    compute_tf_cookbook.scan()
+    assert len(compute_tf_cookbook.words) != 0
+
+
+def test_sort():
+    """Check that the sort function works properly"""
+    compute_tf_cookbook.filter_chars_and_normalize()
+    compute_tf_cookbook.frequencies()
+    compute_tf_cookbook.sort()
+    freqSort = compute_tf_cookbook.word_freqs
+    max_freq = 100000
+    for tf in freqSort:
+        assert tf[1] <= max_freq
+        max_freq = tf[1]

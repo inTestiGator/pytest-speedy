d02f189dc94235fc8bd3ca725fffca5baff5b00a
Matthew Marconi <marconim@aldenv138.allegheny.edu>
2019-04-30 14:33:03 -0400

    add specific emails to author portion of setup.py

--- pytest_speedy/setup.py
+++ pytest_speedy/setup.py
@@ -8,7 +8,7 @@ setup(
     description="A pytest plugin that reorders test cases from fastest to slowest.",
     url="url here",
     author="Matt Marconi, Ben Watto, Dillon Thoma, Mikey Spurr, Steve Li",
-    author_email="email here",
+    author_email="marconim@alleghney.edu, wattob@allegheny.edu, thomad@allegheny.edu, ",
     license="Pytest-Speedy Copyright ",
     py_modules=["pytest_speedy"],
     install_requires=["pytest"],

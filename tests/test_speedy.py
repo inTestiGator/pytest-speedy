a61c103aa3f8f1ad72d4aa5085b385d3c2c3272d
Benjamin Watto <wattob@aldenv140.allegheny.edu>
2019-05-02 15:30:00 -0400

    Pylint Corrections

--- tests/test_speedy.py
+++ tests/test_speedy.py
@@ -1,6 +1,7 @@
 """ Tests the pytest_speedy.py plugin for pytest """
-import pytest
 
+import pytest
+# pylint: disable=unused-imports
 
 # def test_addoption():
 #     """ Tests the addoption function to make sure output is printed in the header correctly """

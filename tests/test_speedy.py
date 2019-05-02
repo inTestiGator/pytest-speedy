0f4d10b6f7815dca305aa8bbc204d82200d9ddff
Dillon Thoma <thomad@aldenv141.allegheny.edu>
2019-05-02 13:49:31 -0400

    Adding sample test to check coverage

--- tests/test_speedy.py
+++ tests/test_speedy.py
@@ -3,3 +3,7 @@
 
 # def test_addoption():
 #     """ Tests the addoption function to make sure output is printed in the header correctly """
+def test_add():
+    """Sample test function"""
+    assert sample.add(1, 1) == 2
+    assert sample.add(0, 1) != 2

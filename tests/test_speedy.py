dee7a723329bebbc1a7cf247263c4ab807e9f0da
Matthew Marconi <matthewmarconi@ewire23-96.allegheny.edu>
2019-05-02 18:47:05 -0400

    delete blank line

--- tests/test_speedy.py
+++ tests/test_speedy.py
@@ -1,7 +1,6 @@
 """ Tests the pytest_speedy.py plugin for pytest """
 
 
-
 # def test_addoption():
 #     """ Tests the addoption function to make sure output is printed in the header correctly """
 def test_add():

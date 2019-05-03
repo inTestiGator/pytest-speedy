47f368b2c4e80f8de91b7c700282ee72c2a24bec
Benjamin Watto <wattob@aldenv126.allegheny.edu>
2019-05-03 14:05:54 -0400

    adding basic tests

--- tests/test_speedy.py
+++ tests/test_speedy.py
@@ -1,7 +1,25 @@
 """ Tests the pytest_speedy.py plugin for pytest """
-# import pytest
+
+import pytest
+import pytest_speedy
 
 
 def test_addoption():
     """test addoption"""
     assert True
+
+
+def test_profile():
+    """Testing profile function within pytest_speedy """
+    tracked_functions = []
+    f = []
+    for func in tracked_functions:
+        globals()[func.__name__] = pytest_speedy.profile(f)
+    assert pytest_speedy.profile(f) is not None
+
+
+def test_sort():
+    """Test sort function within pytest_speedy"""
+    list = [1, 3, 4, 2]
+    sortList = pytest_speedy.sort(list)
+    assert sortList is None
